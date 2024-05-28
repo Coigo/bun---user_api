a0ca49cdb6f92536ee97598813f606197c723691
miguel <migueelcoi@gmail.com>
2024-05-28 12:17:08 -0300

    alter dockerfile

--- app/src/infrastructure/index.ts
+++ app/src/infrastructure/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Client } from "pg";
+import { Client, Pool } from "pg";
 import * as schema from './schema/schema'
 import dotenv from 'dotenv'
 
@@ -15,20 +15,21 @@ if (
     throw new Error('some database params are missing')
 }
 
-console.log(process.env.DB_NAME
+console.log(
+  process.env.DB_NAME
   ," ",process.env.DB_USER
   ," ",process.env.DB_PASSWORD
   ," ",process.env.DB_PORT
   ," ",process.env.DB_HOST);
 
 
-const client = new Client({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+  const client = new Pool({
+    host: process.env.DB_HOST,
+    port:  Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+  });
 
 
 await client.connect();
